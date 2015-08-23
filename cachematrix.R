dce9062fbaea33d62b4ae06d7af266648ca76570
Marcin Wasowski <sieciobywatel@users.noreply.github.com>
2015-08-23 22:55:19 +0200

    [cachematrix] added example of usage

--- cachematrix.R
+++ cachematrix.R
@@ -31,3 +31,37 @@ cacheSolve <- function(x, ...) {
         x$set_solved(inv) # store computed inverse in CacheMatrix
         inv
 }
+
+## Sample usage:
+# > m = matrix(c(4, 2, 2, 2), nrow=2)
+# > m
+#      [,1] [,2]
+# [1,]    4    2
+# [2,]    2    2
+
+# > cacheable = makeCacheMatrix(m)
+# > cacheable$get()
+#      [,1] [,2]
+# [1,]    4    2
+# [2,]    2    2
+
+# > solve(m)
+#      [,1] [,2]
+# [1,]  0.5 -0.5
+# [2,] -0.5  1.0
+
+# > cacheSolve(cacheable)
+#      [,1] [,2]
+# [1,]  0.5 -0.5
+# [2,] -0.5  1.0
+# > cacheSolve(cacheable)
+# getting cached data
+#      [,1] [,2]
+# [1,]  0.5 -0.5
+# [2,] -0.5  1.0
+
+# > cacheSolve(cacheable) == solve(m)
+# getting cached data
+#      [,1] [,2]
+# [1,] TRUE TRUE
+# [2,] TRUE TRUE
